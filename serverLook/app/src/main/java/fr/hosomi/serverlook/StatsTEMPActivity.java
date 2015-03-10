969ed6c451bf389866b02830834374a8176d6a14
kenfire <21304140@etu.unicaen.fr>
2015-03-10 18:24:37 +0100

    Ajout de la 2ème activitée

--- serverLook/app/src/main/java/fr/hosomi/serverlook/StatsTEMPActivity.java
+++ serverLook/app/src/main/java/fr/hosomi/serverlook/StatsTEMPActivity.java
@@ -1,7 +1,41 @@
 package fr.hosomi.serverlook;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 /**
  * Created by kenzo on 10/03/2015.
  */
-public class StatsTEMPActivity extends MainActivity {
+public class StatsTEMPActivity extends ActionBarActivity {
+    static final private int MENU_PREFERENCES = Menu.FIRST;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.add(0, MENU_PREFERENCES, Menu.NONE, R.string.action_settings);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
