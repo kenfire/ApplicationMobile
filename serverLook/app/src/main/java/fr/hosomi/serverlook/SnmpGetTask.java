4708fb0a6014f53e674aacb26f52974eb94c0e2e
Alexandre Maurouard <21300059@etu.unicaen.fr>
2015-03-10 18:58:14 +0100

    Création des classes SNMP

---
+++ serverLook/app/src/main/java/fr/hosomi/serverlook/SnmpGetTask.java
@@ -0,0 +1,8 @@
+package fr.hosomi.serverlook;
+
+/**
+ * Created by Alex on 10/03/15.
+ */
+public class SnmpGetTask {
+    
+}
---
+++ serverLook/app/src/main/java/fr/hosomi/serverlook/SnmpGetTaskSonde.java
@@ -0,0 +1,7 @@
+package fr.hosomi.serverlook;
+
+/**
+ * Created by Alex on 10/03/15.
+ */
+public class SnmpGetTaskSonde {
+}
