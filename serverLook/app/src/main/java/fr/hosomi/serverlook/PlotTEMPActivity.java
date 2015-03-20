37a0e5bd1f3b60e02b675f5527e915035bb1361a
Adrien Clairembault <adrien.clairembault@gmail.com>
2015-03-20 10:20:38 +0100

    Debut activity PlotTEMP
    Bug

--- serverLook/app/src/main/java/fr/hosomi/serverlook/PlotTEMPActivity.java
+++ serverLook/app/src/main/java/fr/hosomi/serverlook/PlotTEMPActivity.java
@@ -5,15 +5,47 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+import java.util.Arrays;
+
 /**
  * Created by Adrien on 10/03/2015.
  */
 public class PlotTEMPActivity extends ActionBarActivity{
 
     static final private int MENU_PREFERENCES = Menu.FIRST;
+    private XYPlot temperature_plot;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plot_temp_activity);
+
+        temperature_plot = (XYPlot) findViewById(R.id.temperature_plot);
+
+        Number[] time = {1, 8, 5, 2, 7, 4};
+        Number[] temperature = {1, 3, 5, 7, 9, 15};
+
+        XYSeries data = new SimpleXYSeries(
+                Arrays.asList(temperature),
+                Arrays.asList(time),
+                "Température baie en °C");
+
+        LineAndPointFormatter dataFormat = new LineAndPointFormatter();
+        dataFormat.setPointLabelFormatter(new PointLabelFormatter());
+        dataFormat.configure(getApplicationContext(),
+                R.xml.line_point_formatter_with_data_format);
+
+        temperature_plot.addSeries(data, dataFormat);
+
     }
 
 
