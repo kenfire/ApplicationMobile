d8610e9c953a24b3464b607253ce28bbde44253f
kenfire <21304140@etu.unicaen.fr>
2015-03-20 11:38:22 +0100

    Commencement affichage de liste

--- serverLook/app/src/main/java/fr/hosomi/serverlook/ArrayUsageTEMPAdapter.java
+++ serverLook/app/src/main/java/fr/hosomi/serverlook/ArrayUsageTEMPAdapter.java
@@ -1,7 +1,19 @@
 package fr.hosomi.serverlook;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 /**
  * Created by kenzo on 10/03/2015.
  */
-public class ArrayUsageTEMPAdapter {
+public class ArrayUsageTEMPAdapter extends ArrayAdapter<TEMP> {
+    private ArrayList<TEMP> objets;
+
+    public ArrayUsageTEMPAdapter(Context context, int textViewResourceId, ArrayList<TEMP> objects) {
+        super(context, textViewResourceId, objects);
+        this.objets = objects;
+    }
+
 }
--- serverLook/app/src/main/java/fr/hosomi/serverlook/StatsTEMPActivity.java
+++ serverLook/app/src/main/java/fr/hosomi/serverlook/StatsTEMPActivity.java
@@ -1,12 +1,19 @@
 package fr.hosomi.serverlook;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  * Created by kenzo on 10/03/2015.
@@ -14,10 +21,19 @@ import android.widget.Button;
 public class StatsTEMPActivity extends ActionBarActivity {
     static final private int MENU_PREFERENCES = Menu.FIRST;
     private Button btn_courbe_stats;
+    private ListView listeView;
+    private Button btnListe;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        btnListe = (Button) findViewById(R.id.btn_list);
         setContentView(R.layout.activity_stats_temp);
+        // Liste température
+        final ArrayList<TEMP> arrayTemp = new ArrayList<>();
+        this.listeView = (ListView) this.findViewById(R.id.listTemp);
+        final ArrayAdapter<TEMP> arrayAdapt;
+        arrayAdapt = new ArrayUsageTEMPAdapter(this, 0, arrayTemp);
+        this.listeView.setAdapter(arrayAdapt);
 
         this.btn_courbe_stats = (Button) findViewById(R.id.btn_courbe_stats);
 
@@ -28,6 +44,51 @@ public class StatsTEMPActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+
+        btnListe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Thread thread = new Thread() {
+                    int state = 0;
+
+                    @Override
+                    public void run() {
+                        try {
+                            arrayF.clear();
+                            ResultSet result = clientBDD.getTableFournisseurs();
+                            while (result.next()) {
+                                String idEnt = result.getString("NF");
+                                String nomEnt = result.getString("nomF");
+                                String statutEnt = result.getString("statut");
+                                String villeEnt = result.getString("ville");
+                                arrayF.add(new Fournisseur(idEnt, nomEnt, statutEnt, villeEnt));
+                            }
+                            result.close();
+                            if (arrayF.isEmpty()) {
+                                arrayF.add(new Fournisseur("", "Vide", "", ""));
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    arrayAdapt.notifyDataSetChanged();
+                                    wait.dismiss();
+                                }
+                            });
+                            AlertDialog.Builder alertB = new AlertDialog.Builder(MainActivity.this);
+                            alertB.setTitle("Changement en cours");
+                            alertB.show();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            wait.dismiss();
+                        }
+                    }
+                };
+                wait.setMessage(getResources().getString(R.string.btn_list));
+                wait.show();
+                thread.start();
+            }
+        });
+
     }
 
 
@@ -53,4 +114,6 @@ public class StatsTEMPActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
